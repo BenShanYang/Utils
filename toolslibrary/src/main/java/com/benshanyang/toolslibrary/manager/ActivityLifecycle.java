201daaf9c4639591d77766bcf326a2a3b9fc516f
BenShanYang <1805197091@qq.com>
2021-01-30 10:46:27 +0800

    完善Activity生命周期管理类

---
+++ toolslibrary/src/main/java/com/benshanyang/toolslibrary/manager/ActivityLifecycle.java
@@ -0,0 +1,44 @@
+package com.benshanyang.toolslibrary.manager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+/**
+ * @ClassName: ActivityLifecycle
+ * @Description: Activity生命周期回调帮助类
+ * @Author: YangKuan
+ * @CreateDate: 2021/1/30 10:14
+ */
+public class ActivityLifecycle {
+
+    public final void onActivityCreated(@NonNull Activity activity, Bundle savedInstanceState) {
+
+    }
+
+    public void onActivityStarted(@NonNull Activity activity) {
+
+    }
+
+    public void onActivityResumed(@NonNull Activity activity) {
+
+    }
+
+    public void onActivityPaused(@NonNull Activity activity) {
+
+    }
+
+    public void onActivityStopped(@NonNull Activity activity) {
+
+    }
+
+    public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+
+    }
+
+    public final void onActivityDestroyed(@NonNull Activity activity) {
+
+    }
+
+}
--- toolslibrary/src/main/java/com/benshanyang/toolslibrary/manager/ActivityStackManager.java
+++ toolslibrary/src/main/java/com/benshanyang/toolslibrary/manager/ActivityStackManager.java
@@ -15,17 +15,17 @@ import java.util.Stack;
  * @Author: YangKuan
  * @Date: 2020/6/30 10:20
  */
-public class ActivityStackManager implements Application.ActivityLifecycleCallbacks {
+public final class ActivityStackManager implements Application.ActivityLifecycleCallbacks {
 
     private final Stack<Activity> activityStack = new Stack<Activity>();
-
     private static ActivityStackManager activityStackManager;
+    private ActivityLifecycle activityLifecycle;
 
     private ActivityStackManager() {
 
     }
 
-    public static ActivityStackManager getInstance() {
+    public final static ActivityStackManager getInstance() {
         if (activityStackManager == null) {
             activityStackManager = new ActivityStackManager();
         }
@@ -33,7 +33,7 @@ public class ActivityStackManager implements Application.ActivityLifecycleCallba
     }
 
     @Override
-    public void onActivityCreated(@NonNull Activity activity, Bundle savedInstanceState) {
+    public final void onActivityCreated(@NonNull Activity activity, Bundle savedInstanceState) {
         //可以在这里add
         activityStack.add(activity);
         Intent intent = activity.getIntent();
@@ -43,37 +43,63 @@ public class ActivityStackManager implements Application.ActivityLifecycleCallba
                 aliveLastActivity(activity);
             }
         }
+        if (activityLifecycle != null) {
+            activityLifecycle.onActivityCreated(activity, savedInstanceState);
+        }
     }
 
     @Override
     public void onActivityStarted(@NonNull Activity activity) {
-
+        if (activityLifecycle != null) {
+            activityLifecycle.onActivityStarted(activity);
+        }
     }
 
     @Override
     public void onActivityResumed(@NonNull Activity activity) {
-
+        if (activityLifecycle != null) {
+            activityLifecycle.onActivityResumed(activity);
+        }
     }
 
     @Override
     public void onActivityPaused(@NonNull Activity activity) {
-
+        if (activityLifecycle != null) {
+            activityLifecycle.onActivityPaused(activity);
+        }
     }
 
     @Override
     public void onActivityStopped(@NonNull Activity activity) {
-
+        if (activityLifecycle != null) {
+            activityLifecycle.onActivityStopped(activity);
+        }
     }
 
     @Override
     public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
-
+        if (activityLifecycle != null) {
+            activityLifecycle.onActivitySaveInstanceState(activity, outState);
+        }
     }
 
     @Override
-    public void onActivityDestroyed(@NonNull Activity activity) {
+    public final void onActivityDestroyed(@NonNull Activity activity) {
         //可以在这里remove
         activityStack.remove(activity);
+        if (activityLifecycle != null) {
+            activityLifecycle.onActivityDestroyed(activity);
+        }
+    }
+
+    /**
+     * 设置Activity生命周期回调帮助类
+     *
+     * @param activityLifecycle Activity生命周期回调帮助类
+     */
+    public final ActivityStackManager setActivityLifecycle(ActivityLifecycle activityLifecycle) {
+        this.activityLifecycle = activityLifecycle;
+        return activityStackManager;
     }
 
     /**
@@ -81,18 +107,19 @@ public class ActivityStackManager implements Application.ActivityLifecycleCallba
      *
      * @return 返回栈顶activity
      */
-    public Activity topActivity() {
+    public final Activity topActivity() {
         return activityStack.peek();
     }
 
     /**
      * 清空栈内所有Activity
      */
-    public void clearStack() {
+    public final ActivityStackManager clearStack() {
         for (Activity activity : activityStack) {
             activity.finish();
         }
         activityStack.clear();
+        return activityStackManager;
     }
 
     /**
